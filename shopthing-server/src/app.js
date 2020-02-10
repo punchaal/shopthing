3c92a2e1299cdb333dc24197e83932daa1818f75
Pratik <punchaal@gmail.com>
2020-02-10 15:45:04 -0500

    Finished adding all needed capabilities for the app

--- shopthing-server/src/app.js
+++ shopthing-server/src/app.js
@@ -1,33 +1,49 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const primeMedians = require('./calculations');
+const bodyParser = express.json();
+const { check, validationResult } = require('express-validator');
+const rateLimit = require('express-rate-limit');
 
-const app = express()
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+app.use(cors());
+app.use(helmet());
 
-app.use(morgan(morganOption))
-app.use(cors())
-app.use(helmet())
+// Rate Limiting
+const limit = rateLimit({
+  max: 100, // max requests
+  windowMs: 60 * 60 * 1000, // 1 Hour of 'ban' / lockout
+  message: 'Too many requests' // message to send
+});
 
-app.get('/',(req,res) => {
-    res.send('Hello, world!')
-})
+// Body Parser
+// app.use(express.json({ limit: '10kb' })); // Body limit is 10
 
-app.use(function errorHandler(error,req,res,next){
-    let response
-    if (NODE_ENV === 'production'){
-        response = {error: {message: 'server error'}}
-    } else{
-        console.error(error)
-        response = {message: error.message, error}
+app
+  .route('/api', limit)
+  .post(
+    bodyParser,
+    [check('upperLimit').isInt({ min: 0, max: 10000000 })],
+    (req, res) => {
+      // Finds the validation errors in this request and wraps them in an object
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      }
+
+      if (!req.body) return res.sendStatus(400);
+
+      let n = req.body.upperLimit;
+      n < 10000000
+        ? res.send(primeMedians(n))
+        : res.status(400).send('Please ensure that the input is valid');
     }
-    res.status(500).json(response)
-})
+  );
 
-module.exports = app
+app.get('/api', (req, res) => {
+  res.send('Hello World');
+});
+
+module.exports = app;
---
+++ shopthing-server/src/calculations.js
@@ -0,0 +1,30 @@
+let primeMedians = function(n) {
+  // Eratosthenes algorithm to find all primes under n
+  let array = [],
+    upperLimit = Math.sqrt(n),
+    output = [];
+
+  // Make an array from 2 to (n - 1)
+  for (let i = 0; i < n; i++) {
+    array.push(true);
+  }
+
+  // Remove multiples of primes starting from 2, 3, 5,...
+  for (let i = 2; i <= upperLimit; i++) {
+    if (array[i]) {
+      for (let j = i * i; j < n; j += i) {
+        array[j] = false;
+      }
+    }
+  }
+
+  for (let i = 2; i < n; i++) {
+    array[i] && output.push(i);
+  }
+
+  //Calculate the median from the sorted output array
+  let mid = Math.floor(output.length / 2);
+  return output.length % 2 ? [output[mid]] : [output[mid - 1], output[mid]];
+};
+
+module.exports = primeMedians;
